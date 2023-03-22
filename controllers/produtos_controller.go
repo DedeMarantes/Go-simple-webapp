62ed8974e9165152501612bae194f760c796e975
DedeMarantes <andre.marantes@gmail.com>
2023-03-21 21:16:55 -0300

    Editando e atualizando produtos

--- controllers/produtos_controller.go
+++ controllers/produtos_controller.go
@@ -38,3 +38,40 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, "/", 301)
 }
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	productID := r.URL.Query().Get("id")
+	models.DeletaProduto(productID)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	templ.ExecuteTemplate(w, "edit", nil)
+	productId := r.URL.Query().Get("id")
+	models.EditaProduto(productId)
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão de id para int: ", err)
+		}
+		precoFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão de preco para float: ", err)
+		}
+		quantidadeInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão de quantidade para int: ", err)
+		}
+		models.AtualizaProduto(idInt, nome, descricao, precoFloat, quantidadeInt)
+	}
+	http.Redirect(w, r, "/", 301)
+}
--- models/produto.go
+++ models/produto.go
@@ -14,7 +14,7 @@ type Produto struct {
 
 func GetAllProducts() []Produto {
 	db := db.ConnectDb()
-	allProducts, err := db.Query("SELECT * FROM produtos")
+	allProducts, err := db.Query("SELECT * FROM produtos ORDER BY id ASC")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -30,6 +30,7 @@ func GetAllProducts() []Produto {
 			panic(err.Error())
 		}
 		//adicionar produtos
+		produto.Id = id
 		produto.Nome = nome
 		produto.Descricao = descricao
 		produto.Preco = preco
@@ -48,5 +49,51 @@ func CreateProduct(nome string, descricao string, preco float64, quantidade int)
 		panic(err.Error())
 	}
 	insertData.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletaProduto(id string) {
+	db := db.ConnectDb()
+	deletarProduto, err := db.Prepare("DELETE FROM produtos WHERE id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarProduto.Exec(id)
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConnectDb()
+	produto, err := db.Query("SELECT * from produtos WHERE id=$1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+	novoProduto := Produto{}
+
+	for produto.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+		err := produto.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+		novoProduto.Id = id
+		novoProduto.Nome = nome
+		novoProduto.Descricao = descricao
+		novoProduto.Preco = preco
+		novoProduto.Quantidade = quantidade
+	}
+	defer db.Close()
+	return novoProduto
+}
+
+func AtualizaProduto(id int, nome string, descricao string, preco float64, quantidade int) {
+	db := db.ConnectDb()
+	atualizaProduto, err := db.Prepare("UPDATE produtos SET nome=$1, descricao=$2, preco=$3, quantidade=$4 WHERE id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+	atualizaProduto.Exec(nome, descricao, preco, quantidade, id)
 	defer db.Close()
 }
